3cdd7d0fe0ac69e9e1e7b2caf299d4a969dd4734
eduardo.souza <eduardo.souza@hbsis.com.br>
2019-11-19 16:38:27 -0300

    # fixed IEventThrower exception

--- SagaTheFlow.Contracts/InboundOrderAvailableFlow/CheckTasksCreated.cs
+++ SagaTheFlow.Contracts/InboundOrderAvailableFlow/CheckTasksCreated.cs
@@ -1,7 +1,14 @@
-﻿namespace SagaTheFlow.Contracts.InboundOrderAvailableFlow
+﻿using System;
+using TheFlow.CoreConcepts;
+using TheFlow.Elements.Events;
+
+namespace SagaTheFlow.Contracts.InboundOrderAvailableFlow
 {
-    public class CheckTasksCreated
+    public class CheckTasksCreated : IEventThrower
     {
-        
+        public void Throw(ExecutionContext context)
+        {
+            Console.WriteLine("Dunno...");
+        }
     }
 }
