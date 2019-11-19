891bd4bd599029b62e83a2bacf50c68a95bd4dd6
eduardo.souza <eduardo.souza@hbsis.com.br>
2019-11-19 14:34:38 -0300

    added process manager holder

---
+++ SagaTheFlow.TheFlow/ProcessManagerHolder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TheFlow;
+using TheFlow.Infrastructure.Stores;
+
+namespace SagaTheFlow.TheFlow
+{
+    public static class ProcessManagerHolder
+    {
+        private static readonly Lazy<ProcessManager> LazyProcessManager =
+            new Lazy<ProcessManager>(() =>
+            {
+                var models = new InMemoryProcessModelsStore();
+                var instances = new InMemoryProcessInstancesStore();
+                return new ProcessManager(models, instances);
+            });
+
+        public static ProcessManager Instance => LazyProcessManager.Value;
+    }
+}
